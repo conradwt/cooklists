65c607abf7a001ab4e9326e760c67c30537f9372
Conrad Taylor <conradwt@gmail.com>
2019-08-07 20:17:20 -0700

    Add landing page controller, view, and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'homes/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#index'
 end
