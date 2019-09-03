f4ce430e8bab35686eed693a1f6de4e0cada4476
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-09-03 20:46:31 +0000

    updated default urls for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -60,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # customizations
+
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
 end
