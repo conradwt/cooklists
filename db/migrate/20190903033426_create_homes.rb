358960b6b0257faaae840fbad04a6e85b846cf20
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-09-03 03:43:26 +0000

    configured database

---
+++ db/migrate/20190903033426_create_homes.rb
@@ -0,0 +1,10 @@
+class CreateHomes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :homes do |t|
+      t.text :name
+      t.text :link
+      t.text :description
+      t.timestamps
+    end
+  end
+end
