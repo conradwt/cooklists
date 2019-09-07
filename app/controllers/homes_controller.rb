b9abbd283dbed60e0bdf1bd8a718b0110021d692
Sean Cook <you@sean.bernard.cook@gmail.com>
2019-09-07 14:45:28 +0000

    added validation rule

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
   def index
+    def create
+      @home = Home.create(home_params)
+      if @home.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
   end
 end
